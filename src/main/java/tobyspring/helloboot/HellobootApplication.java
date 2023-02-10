d44e834b84a14bd63af0a76ea2ed277397698cf4
mmtos <js223kr92@naver.com>
2023-02-10 14:00:52 +0900

    embed 톰캣을 실행시키기

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,13 +1,16 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
 
-@SpringBootApplication
 public class HellobootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+	public static void main(String[] args) throws Exception {
+		// 1. embed 톰캣을 실행시키기
+//		new Tomcat().start();
+		TomcatServletWebServerFactory servletWebServerFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = servletWebServerFactory.getWebServer();
+		webServer.start();
 	}
-
 }
