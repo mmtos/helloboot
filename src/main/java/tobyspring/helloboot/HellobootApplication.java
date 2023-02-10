ea9b4635066bde51cdf476d418f6f8ec87bc4a9e
mmtos <js223kr92@naver.com>
2023-02-10 16:05:57 +0900

    annotation 기반 설정을 사용한 스프링 컨테이너로 변경

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,30 +1,31 @@
 package tobyspring.helloboot;
 
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+// 스프링 컨테이너가 이안에 자바코드 설정정보가 있다는걸 알게 만들어야함
+@Configuration
 public class HellobootApplication {
+
+    @Bean
+    public HelloService helloService(){
+       return  new SimpleHelloService();
+    }
+
+    // factory method를 이용해서 빈 구성정보 정의
+    @Bean
+    public HelloController helloController(HelloService helloService){
+        return new HelloController(helloService);
+    }
 
     public static void main(String[] args) {
         // 스프링 컨테이너 refresh 시점에 톰캣 실행과정 통합
-        GenericWebApplicationContext gwac = new GenericWebApplicationContext(){
+        AnnotationConfigWebApplicationContext acwac = new AnnotationConfigWebApplicationContext(){
             @Override
             protected void onRefresh() {
                 super.onRefresh();
@@ -41,10 +42,13 @@ public class HellobootApplication {
         // Assembler : 구체클래스간의 결합을 인터페이스로 약화시키고, 런타임 시점에 구체클래스를 사용할 수 있도록 DI해주는 역할을 말한다.
         // 스프링 컨테이너는 Assembler의 역할을 하고 있다.
         // Assembler의 구현 방법 1 : 생성자로 DI해주는 방법, Factory 호출시 전달, setter를 통해 주입
-        gwac.registerBean(HelloController.class);
-        gwac.registerBean(SimpleHelloService.class);
-        gwac.refresh(); // 이때 bean을 생성함. template method라서 훅을 추가할 수 있다.
+//        acwac.registerBean(HelloController.class);
+//        acwac.registerBean(SimpleHelloService.class);
+//        acwac.refresh(); // 이때 bean을 생성함.
 
+        //annotation 기반 설정을 사용한 스프링 컨테이너는 더이상 registerBean을 사용하지 못함
+        acwac.register(HellobootApplication.class);
+        acwac.refresh();
 
     }
 
