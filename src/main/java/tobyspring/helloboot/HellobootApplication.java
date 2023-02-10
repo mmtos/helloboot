5738a2fa905c49469e769a2bef45d8c0f7fecef7
mmtos <js223kr92@naver.com>
2023-02-10 14:39:59 +0900

    커넥터 추가, 서블릿 추가

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,6 +1,11 @@
 package tobyspring.helloboot;
 
-import org.apache.catalina.startup.Tomcat;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 
@@ -8,9 +13,25 @@ public class HellobootApplication {
 
 	public static void main(String[] args) throws Exception {
 		// 1. embed 톰캣을 실행시키기
-//		new Tomcat().start();
-		TomcatServletWebServerFactory servletWebServerFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = servletWebServerFactory.getWebServer();
+		TomcatServletWebServerFactory servletWebServerFactory = new TomcatServletWebServerFactory(8081);
+		Connector anotherConnector = new Connector("HTTP/1.1");
+		anotherConnector.setPort(8081);
+		servletWebServerFactory.addAdditionalTomcatConnectors(anotherConnector);
+		servletWebServerFactory.addConnectorCustomizers(connector -> {
+			connector.setPort(8080);
+		});
+		WebServer webServer = servletWebServerFactory.getWebServer(servletContext -> {
+			// 서블릿 추가 및 URL 매핑
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp)
+						throws ServletException, IOException {
+					resp.setStatus(HttpServletResponse.SC_OK);
+					resp.setHeader("Content-Type", "text/plain");
+					resp.getWriter().println("Hello Servlet");
+				}
+			}).addMapping("/hello");
+		});
 		webServer.start();
 	}
 }
