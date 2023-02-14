f1269dea025612bd3419b65b29e713d0cd763dea
mmtos <js223kr92@naver.com>
2023-02-14 15:57:52 +0900

    @Import를 사용해 외부 설정파일을 컨테이너 구성에 사용

--- src/main/java/tobyspring/config/Config.java
+++ src/main/java/tobyspring/config/Config.java
@@ -1,4 +1,4 @@
-package tobyspring.helloboot;
+package tobyspring.config;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
--- src/main/java/tobyspring/helloboot/MySpringBootAnnotation.java
+++ src/main/java/tobyspring/helloboot/MySpringBootAnnotation.java
@@ -6,11 +6,17 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import tobyspring.config.Config;
 
 // RetentionPolicy.CLASS 클래스파일까진 남아있지만 로딩 후엔 남아있지 않음
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @ComponentScan
 @Configuration
+// 스프링 3.0부터 있었음 파라미터로 넘겨진 @Component가 붙은 클래스를
+// 컨테이너 구성정보에 추가
+// @Import를 이용하면 컴포넌트 스캔 바깥에 있는 @Configuration 클래스를 사용할 수 있다!
+@Import(Config.class)
 public @interface MySpringBootAnnotation {
 }
