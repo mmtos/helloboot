ac4cbd8ad8643a4591fa9f3027be7ec4b9a97c8f
mmtos <js223kr92@naver.com>
2023-02-14 15:50:01 +0900

    빈 구분 설명 추가

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -10,6 +10,13 @@ import org.springframework.web.servlet.DispatcherServlet;
 
 @MySpringBootAnnotation
 public class HellobootApplication {
+    /*
+    빈의 구분
+    애플리케이션 빈 : 개발자가 직접 제공
+        애플리케이션 로직 빈(by component scan)
+        애플리케이션 인프라스트럭쳐 빈(by auto configuration) : dispatcher servlet, tomcat servlet web server factory
+    컨테이너 인프라스트럭쳐 빈(스프링의 기능을 제공하기 위한 빈들), 부트스트래핑 과정에서 자동 등록
+    */
     public static void main(String[] args) {
         SpringApplication.run(HellobootApplication.class, args);
     }
