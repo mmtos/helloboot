6af3663df7268fbecd8eeae6721b330aa04b9714
mmtos <js223kr92@naver.com>
2023-02-14 16:02:12 +0900

    설정파일 분리

--- src/main/java/tobyspring/helloboot/MySpringBootAnnotation.java
+++ src/main/java/tobyspring/helloboot/MySpringBootAnnotation.java
@@ -7,7 +7,8 @@ import java.lang.annotation.Target;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import tobyspring.config.Config;
+import tobyspring.config.autoconfig.DispatcherServletConfig;
+import tobyspring.config.autoconfig.TomcatWebServerConfig;
 
 // RetentionPolicy.CLASS 클래스파일까진 남아있지만 로딩 후엔 남아있지 않음
 @Retention(RetentionPolicy.RUNTIME)
@@ -17,6 +18,9 @@ import tobyspring.config.Config;
 // 스프링 3.0부터 있었음 파라미터로 넘겨진 @Component가 붙은 클래스를
 // 컨테이너 구성정보에 추가
 // @Import를 이용하면 컴포넌트 스캔 바깥에 있는 @Configuration 클래스를 사용할 수 있다!
-@Import(Config.class)
+@Import({TomcatWebServerConfig.class, DispatcherServletConfig.class})
+// 최상위에 설정파일들을 계속 추가하는 방식은 파일 내용이 계속 길어지는 문제가 있음.
+// 가독성도 떨어짐
+
 public @interface MySpringBootAnnotation {
 }
