65a577ad467478453454ee9800515e4191eea900
mmtos <js223kr92@naver.com>
2023-02-14 16:39:02 +0900

    ImportSelector에서 AutoConfiguration 사용하도록 하기, 후보 클래스들은 외부 파일로 설정

--- src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
+++ src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
@@ -1,15 +1,31 @@
 package tobyspring.config;
 
+import java.util.stream.StreamSupport;
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
-public class MyAutoConfigImportSelector implements DeferredImportSelector {
+public class MyAutoConfigImportSelector implements DeferredImportSelector, BeanClassLoaderAware {
+
+    private ClassLoader classLoader;
+
+    // BeanClassLoaderAware 인터페이스를 구현한 경우 setter로 BeanClassLoader를 주입받을 수 있다.
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+//    // BeanClassLoaderAware을 구현하지 않고 생성자로 받아도 됨
+//    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+//        this.classLoader = classLoader;
+//    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        // DB나 외부 설정파일에서 가져올 수 있다.
-        return new String[] {
-                "tobyspring.config.autoconfig.DispatcherServletConfig",
-                "tobyspring.config.autoconfig.TomcatWebServerConfig"
-        };
+        Iterable<String> candidates = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+        return StreamSupport
+                .stream(candidates.spliterator(),false)
+                .toArray(String[]::new);
     }
 }
