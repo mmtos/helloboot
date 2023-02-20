b11cce3d0944e197848825558aaf19908bf35c32
mmtos <js223kr92@naver.com>
2023-02-20 14:18:30 +0900

    @Conditional로 Jetty 웹서버빈 선택

--- src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
@@ -1,29 +1,30 @@
 package tobyspring.config.autoconfig;
 
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Conditional;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import tobyspring.config.MyAutoConfiguration;
 import tobyspring.config.autoconfig.JettyWebServerConfig.JettyCondition;
-import tobyspring.config.autoconfig.TomcatWebServerConfig.TomcatCondition;
 
 @MyAutoConfiguration
-@Conditional(TomcatCondition.class)
-public class TomcatWebServerConfig {
-    @Bean("tomcatWebServerFactory")
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+@Conditional(JettyCondition.class)
+public class JettyWebServerConfig {
+    @Bean
+    // 빈의 이름은 메소드 이름을 따라간다.
+    public ServletWebServerFactory jettyWebServerFactory() {
+        return new JettyServletWebServerFactory();
     }
 
-    static class TomcatCondition implements Condition {
+
+    static class JettyCondition implements Condition {
         @Override
         public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return false;
+            return true;
         }
     }
 }
