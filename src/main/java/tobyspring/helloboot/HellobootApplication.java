dc33448b6da442a22614425592e376fb66c5b9ec
mmtos <js223kr92@naver.com>
2023-02-20 17:02:26 +0900

    단순한 프로퍼티 값으로 빈 세부설정하기

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,6 +1,9 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import tobyspring.config.MySpringBootApplication;
 
 @MySpringBootApplication
@@ -10,4 +13,15 @@ public class HellobootApplication {
         SpringApplication.run(HellobootApplication.class, args);
     }
 
+    // ApplicationRunner
+    // context완성 후 실행됨.
+    @Bean
+    ApplicationRunner applicationRunner(Environment env){
+        // Environment Abstraction과 우선순위
+        // 일반적으로 application.yml에 디폴트 프로퍼티, 시스템 환경변수로 오버라이딩 하는 방식을 주로 사용한다.
+        return args -> {
+            String name = env.getProperty("my.name");
+            System.out.println("my name is " + name);
+        };
+    }
 }
